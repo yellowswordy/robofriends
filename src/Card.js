ac3872af306d6b6ef3fec5d653e4625b0b79d27c
Nikita Kozlov <yellowswordy@yandex.com>
2019-09-30 23:35:19 -0700

    Create card functional component

---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React  from 'react';
+
+const Card =() =>{
+
+        return (
+            <div>
+
+                <div>
+                    <img src="https://robohash.org/1?200x200" alt="robots"/>
+                    <div>
+                        <h2>Jane Doe</h2>
+                        <p>jane.doe@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+
+export default Card;
