ab543e34ad3f2671a49f5b5794ccf277e02e8e21
Nikita Kozlov <yellowswordy@yandex.com>
2019-10-01 06:04:33 -0700

    implement Scroll functionality through props.children

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import './App.css'
+import Scroll from "./Scroll";
 
 
 class App extends Component{
@@ -44,7 +45,10 @@ class App extends Component{
                 <div className='tc'>
                     <h1 className='f2'>Robofriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filterRobots}/>
+
+                    <Scroll>
+                        <CardList robots={filterRobots}/>
+                    </Scroll>
                 </div>
 
             )
---
+++ src/Scroll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scroll = (props) => {
+
+        return (
+           <div style={{overflowY: 'scroll', border: '3px solid black', height: '700px'}}>
+               {props.children}
+           </div>
+        );
+    }
+
+
+export default Scroll;
