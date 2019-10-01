4cb4e2e6f3cab1ac2ab03e640c97b4bd8761be83
Nikita Kozlov <yellowswordy@yandex.com>
2019-10-01 00:11:43 -0700

    Add CardList component. Render all robotcards on page

--- src/index.js
+++ src/index.js
@@ -4,16 +4,11 @@ import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
-import Card from "./Card";
+import CardList from "./CardList";
 import {robots} from './Robots'
 
 ReactDOM.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-    </div>, document.getElementById('root'));
+    <CardList robots={robots}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
